f22b4863fca7c4ca8a4ee13fe89807e02affebf1
Boilerr <sergey.nys@gmail.com>
2025-02-24 19:09:00 +0200

    Add Spring Boot 3.4.3 template from start.spring.io

---
+++ src/main/java/weather/WeatherRestSpringApplication.java
@@ -0,0 +1,13 @@
+package weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRestSpringApplication.class, args);
+	}
+
+}
