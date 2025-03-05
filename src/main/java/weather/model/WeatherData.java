fdf288e091f23344750ceb5e9d5595a5ee9383f3
Boilerr <sergey.nys@gmail.com>
2025-03-05 19:29:41 +0200

    Add WeatherData

--- src/main/java/weather/model/WeatherData.java
+++ src/main/java/weather/model/WeatherData.java
@@ -2,6 +2,9 @@ package weather.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
+
 
 @Entity
 @Table(name = "weather")
@@ -10,5 +13,99 @@ public class WeatherData {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "lat")
+    private Double lat;
+
+    @Column(name = "lon")
+    private Double lon;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+
+    @Column(name = "temperatures")
+    private float[] temperatures;
+
+
+    // Empty Constructor
+    public WeatherData() {
+    }
+
+    // Constructor without id
+    public WeatherData(LocalDate date, Double lat, Double lon, String city, String state, float[] temperatures) {
+        this.date = date;
+        this.lat = lat;
+        this.lon = lon;
+        this.city = city;
+        this.state = state;
+        this.temperatures = temperatures;
+    }
+
+
+    // Getters and Setters for all fields
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public float[] getTemperatures() {
+        return temperatures;
+    }
+
+    public void setTemperatures(float[] temperatures) {
+        this.temperatures = temperatures;
+    }
+
 
 }
